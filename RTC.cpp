8d32994dd3d206c8005a583d8a2242c49613b902
Irsyad <git-123.evaluator748@slmails.com>
2024-09-28 08:13:05 +0000

    Add all tm struct variables

--- RTC.cpp
+++ RTC.cpp
@@ -128,73 +128,115 @@ void _set(uint8_t ss, uint8_t mm, uint8_t hh, uint8_t ww, uint8_t d, uint8_t m, 
   Wire.endTransmission();
 }
 
-void RTC::set(struct tm * tmp) {
-  uint8_t wday = tmp -> tm_wday;
-  if (wday == 0) {
-    wday = 7;
-  }
-  _set(tmp -> tm_sec, tmp -> tm_min, tmp -> tm_hour, wday, tmp -> tm_mday, tmp -> tm_mon + 1, tmp -> tm_year - 100);
+uint8_t daysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+uint8_t daysInMonthLeap[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+#define _LEAP_YEAR(Y) (!(((Y)) % 4) && ((((Y)) % 100) || !(((Y)) % 400)))
+
+void RTC::set(struct tm* tmp) {
+	static uint8_t d,m;
+
+	if(tmp->tm_mon == 0 && tmp->tm_mday == 0){
+		static uint16_t i = tmp->tm_yday;
+		
+		while(i > 31){
+        		if(_LEAP_YEAR(tmp->tm_year+1900) == 1){
+            			i-=daysInMonthLeap[m];
+			} else {
+				i-=daysInMonth[m];
+			}
+			m++;
+    		}
+
+		d=i+1;
+		m+=1;
+
+	} else {
+		d = tmp->tm_mday;
+		m = tmp->tm_mon+1;
+	}
+
+	uint8_t wday = tmp->tm_wday;
+	uint16_t y = tmp->tm_year - 100;	
+
+	if (wday == 0) {
+		wday = 7;
+	}
+	_set(tmp->tm_sec, tmp->tm_min, tmp->tm_hour, wday, d, m, y);
 }
 
-void RTC::now_r(struct tm * tmp) {
+int16_t _yearday(int32_t y, uint8_t m, uint8_t d){
+	uint16_t days = d;
+    	for (uint8_t i = 1; i < m; i++){
+		days += daysInMonth[i - 1];
+	}	
+	if (m > 2 && _LEAP_YEAR(y) == 1){
+		days++;
+    	}
+	return days - 1;
+}
+
+void RTC::now_r(struct tm* tmp) {
 	uint8_t second, minute, hour, dayOfWeek, dayOfMonth, month;
 	uint16_t year;
 
-  Wire.beginTransmission(_DS_I2C_ADDRESS);
+	Wire.beginTransmission(_DS_I2C_ADDRESS);
 
-  Wire.write(0);
+	Wire.write(0);
 
-  Wire.endTransmission();
+	Wire.endTransmission();
 
-  Wire.requestFrom(_DS_I2C_ADDRESS, 7);
+	Wire.requestFrom(_DS_I2C_ADDRESS, 7);
 
-  second = bcdToDec(Wire.read() & 0x7f);
-  minute = bcdToDec(Wire.read());
-  hour = bcdToDec(Wire.read() & 0x3f);
+ 	second = bcdToDec(Wire.read() & 0x7f);
+	minute = bcdToDec(Wire.read());
+	hour = bcdToDec(Wire.read() & 0x3f);
 
-  dayOfWeek = bcdToDec(Wire.read());
+	dayOfWeek = bcdToDec(Wire.read());
 
-  dayOfMonth = bcdToDec(Wire.read());
-  month = bcdToDec(Wire.read());
-  year = bcdToDec(Wire.read());
+	dayOfMonth = bcdToDec(Wire.read());
+	month = bcdToDec(Wire.read());
+	year = bcdToDec(Wire.read());
 
-  if (hour > 23) {
-    second = 1;
-    minute = 1;
-    hour = 1;
+ 	if (hour > 23) {
+		second = 1;
+		minute = 1;
+ 		hour = 1;
 
-    dayOfWeek = 1;
+ 		dayOfWeek = 1;
 
-    dayOfMonth = 1;
-    month = 1;
-    year = 1;
-  }
+		dayOfMonth = 1;
+		month = 1;
+		year = 1;
+	}
 
-  #define _LEAP_YEAR(Y)(!(((Y)) % 4) && ((((Y)) % 100) || !(((Y)) % 400)))
+	if (_LEAP_YEAR(year + 2000) == 0 && dayOfMonth == 29 && month == 2) {
+		_set(second, minute, hour, dayOfWeek, 1, 3, year);
+	}
 
-  if (_LEAP_YEAR(year + 2000) == 0 && dayOfMonth == 29 && month == 2) {
-    _set(second, minute, hour, dayOfWeek, 1, 3, year);
-  }
+	if (year == 0) {
+ 		_set(second, minute, hour, dayOfWeek, dayOfMonth, month, 100);
+	}
 
-  if (year == 0) {
-    _set(second, minute, hour, dayOfWeek, dayOfMonth, month, 100);
-  }
+	uint8_t wday = dayOfWeek;
 
-  uint8_t wday = dayOfWeek;
+	if (wday == 7) {
+		wday = 0;
+	}
 
-  if (wday == 7) {
-    wday = 0;
-  }
+	tmp->tm_sec = second;
+	tmp->tm_min = minute;
+	tmp->tm_hour = hour;
 
-  tmp -> tm_sec = second;
-  tmp -> tm_min = minute;
-  tmp -> tm_hour = hour;
+	tmp->tm_wday = wday;
 
-  tmp -> tm_wday = wday;
+	tmp->tm_mday = dayOfMonth;
+	tmp->tm_mon = month - 1;
+	tmp->tm_year = year + 100;
+	
+	tmp->tm_yday = _yearday(year+2000, month, dayOfMonth);
 
-  tmp -> tm_mday = dayOfMonth;
-  tmp -> tm_mon = month - 1;
-  tmp -> tm_year = year + 100;
+	tmp->tm_isdst = 0;
 }
 
 struct tm _return;
@@ -236,10 +278,10 @@ uint32_t RTC::millis() {
   if (_sqw == -1) {
     _msec = 0;
   } else {
-    if(count){
+		if(count){
     	_msec = 0;
     	count = 0;
-    } else {
+  	} else {
     	static uint32_t prev;
     	if(micros() - prev >= 1000){
       	_msec++;
